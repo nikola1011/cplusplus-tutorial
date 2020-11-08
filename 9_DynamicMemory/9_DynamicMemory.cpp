087d5bb930e4d85a1f5be489f226812d01cee674
nikola1011 <43878143+nikola1011@users.noreply.github.com>
2020-11-08 22:06:16 +0100

    Finish dynamic memory (new and delete keywords).
    Add Bad programming and Homework section.

--- 9_DynamicMemory/9_DynamicMemory.cpp
+++ 9_DynamicMemory/9_DynamicMemory.cpp
@@ -1,20 +1,57 @@
-// 9_DynamicMemory.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+// All memory needs determined before the program execution is located on stack. ("static" memory)
+// However, memory can be allocated in runtime (while program is executing), thus making this memory "dynamic".
+// Dynamic memory is allocated on a structure called heap.
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Static array - because array length is known at compile time
+    int myStaticArray[32];
+
+    int arrayLength = 0;
+    cout << "Enter array length: " << endl;
+    cin >> arrayLength;
+    // Dynamic array - size of needed memory is known at runtime
+    int* dynamicArray; // Declare a pointer for a array
+    // Allocate memory for array by using 'new' keyword.
+    dynamicArray = new int[arrayLength]; // There's no guarantee that the requested memory will be granted (memory is limited, it is handled by OS)
+
+    cout << "Array values: " << endl;
+    for (int i = 0; i < arrayLength; i++)
+        cout << dynamicArray[i] << " "; // Random memory values
+
+    // A memory for a single element can also be allocated
+    int* dynamicInt = new int;
+    cout << "\nSingle element memory: " << *dynamicInt << endl;
+
+    // The allocated memory is the responsibility of the program.
+    // When the program allocates memory it should also deallocate it (free it for other programs to use it).
+    // This is done with 'delete' keyword.
+    delete dynamicInt;
+    delete [] dynamicArray;
+
+    // 'delete' does not immediately free memory for performance reasons.
+    // But accessing freed memory causes undefined behaviour. A runtime error can or may not occur.
+    // After 'delete' statement the program is promising that it will no longer use that memory, or the pointer that points to that memory.
+    // If that promise is not kept, program has undefined consequences.
+    /// BAD PROGRAMMING
+    // cout << dynamicInt << " " << *dynamicInt << endl;
+    // *dynamicInt = 3;
+    // dynamicArray[1] = 3;
+    /// BAD PROGRAMMING
+
+    /// HOMEWORK
+    // Create a loop that allocates and does not free array
+        // See program's memory consumption in Task Manager (CTRL + SHIFT + ESC to open Task Manager window, with your program and see memory consumption)
+    // Apply the same allocating logic but free memory
+        // Notice the difference
+    // What happens if you delete NULL pointer
+        // int* x = NULL;
+        // delete x;
+        // What will happen ?
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
