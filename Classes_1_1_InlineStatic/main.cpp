a0ffaf8cca2c526b435e441ff1fbc39d57343a07
nikola1011 <43878143+nikola1011@users.noreply.github.com>
2020-11-09 20:00:40 +0100

    Classes - inline and static methods

---
+++ Classes_1_1_InlineStatic/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Calendar.h"
+
+// Header and source files - .h and .cpp files
+//C++ classes (and often function prototypes) are normally split up into two files.
+//The header file has the extension of .h and contains class definitions and functions.
+//The implementation of the class goes into the .cpp file.
+//By doing this, if your class implementation doesn’t change then it won’t need to be recompiled.
+//Also it keep the code clean and readable.
+
+using namespace std;
+
+int main()
+{
+    // Initialize static variable (notice the class 'Calendar::', not object association)
+    // Initialization must be done outside the class.
+    Calendar::yearZero = 1970; // Unix time year
+    cout << "Year zero is: " << Calendar::yearZero << endl;
+
+    // Call static function
+    Calendar::getCurrentTime();
+
+    Calendar c; // Define object
+    c.setMyFavoriteMonth();
+    cout << "Your favorite month is: " << c.getMyFavoriteMonth() << endl;
+
+    c.setMyFavoriteDay("Tuesday");
+    c.getMyFavoriteDay();
+
+    return 0;
+}
