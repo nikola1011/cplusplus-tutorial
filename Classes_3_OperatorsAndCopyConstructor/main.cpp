263ac6b06d651ddd2b1c6f0ad702af75836707ae
wolfc <wolfc@GPU-MACHINE>
2020-11-21 22:54:30 +0100

    OperatorsAndCopyConstructors project creation. TODO section.

---
+++ Classes_3_OperatorsAndCopyConstructor/main.cpp
@@ -0,0 +1,19 @@
+// TODO:
+//primer.vektor
+//
+//copy constructor
+//
+//dinamic allocation in operator=
+//
+//operatori
+//istream& operator>>
+//ostream & operator<<
+//
+//proucite -> "D:\OOP\OOP RV04 Primeri\C0403"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
