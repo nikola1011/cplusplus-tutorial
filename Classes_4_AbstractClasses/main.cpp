f3635c91248ce4bb9626fbe1d1db3c3bea260ada
Nikola <Nikola@HP-LAPTOP>
2020-12-15 01:03:35 +0100

    Abstract classes project.

---
+++ Classes_4_AbstractClasses/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+// Abstract class - if a class contains at LEAST ONE pure virtual function, it is an abstract class
+class Polygon 
+{
+protected:
+    int width, height;
+
+public:
+    void setValues(int a, int b)
+    {
+        width = a; height = b;
+    }
+
+    // virtual <function declaration> = 0;
+    // Pure virtual function
+    virtual int area() = 0;
+
+    void printArea()
+    {
+        // Pure virtual function can be called inside abstract class
+        cout << this->area() << '\n';
+    }
+};
+
+class Rectangle : public Polygon 
+{
+public:
+    int area(void)
+    {
+        return (width * height);
+    }
+};
+
+class Triangle : public Polygon 
+{
+public:
+    int area(void)
+    {
+        return (width * height / 2);
+    }
+};
+
+int main()
+{
+    // Abstract classes cannot be instantiated
+    // Polygon p; // Compiler error
+    // Polygon p = new Polygon(); // Compiler error
+    
+    Rectangle rect;
+    Triangle triangle;
+    
+    Polygon* ppoly1 = &rect; // Pointer of abstract class can exist
+    Polygon* ppoly2 = &triangle;
+
+    ppoly1->setValues(4, 5);
+    ppoly2->setValues(4, 5);
+    ppoly1->printArea();
+    ppoly2->printArea();
+
+    return 0;
+}
